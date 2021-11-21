4cda33b52e86b58952b7611cd03344799a812cb4
liuzhihaoliuzhihao_i <liuzhihaoliuzhihao_i@didiglobal.com>
2021-11-21 18:28:04 +0800

    餐厅总览页面开发完成

--- src/HeyCafe/components/Table.vue
+++ src/HeyCafe/components/Table.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="table-container">
-    <a-popover title="Title" trigger="click">
+    <a-popover :title="`No. ${deskInfo.id}`" trigger="click">
       <template #content>
-        <p>Content</p>
-        <p>Content</p>
+        <p>{{ $t('hc.orderID') }}：{{ deskInfo.orderId }}</p>
+        <p>{{ $t('hc.currentWaiter') }}：{{ deskInfo.waiterId }}</p>
+        <div>
+          <span>{{ $t('hc.tableStatus') }}：</span>
+          <span v-if="deskInfo.status===-1" :class="mapClass(deskInfo.status)">{{ $t('hc.noCustomers') }}</span>
+          <span v-if="deskInfo.status===0" :class="mapClass(deskInfo.status)">{{ $t('hc.needWaiter') }}</span>
+          <span v-else-if="deskInfo.status===1" :class="mapClass(deskInfo.status)">{{ $t('hc.waitingForMeal') }}</span>
+          <span v-else-if="deskInfo.status===2" :class="mapClass(deskInfo.status)">{{ $t('hc.eating') }}</span>
+          <span v-else-if="deskInfo.status===3" :class="mapClass(deskInfo.status)">{{ $t('hc.needClean') }}</span>
+        </div>
+        <a-button @click="deskInfo.seats < 8 && deskInfo.seats++">{{ $t('hc.addChair') }}</a-button>
+        <a-button @click="deskInfo.seats > 0 && deskInfo.seats--">{{ $t('hc.removeChair') }}</a-button>
+        <a-button @click="$emit('remove-table',deskInfo.id)">{{ $t('hc.removeTable') }}</a-button>
+        <a-button v-if="deskInfo.status === -1" @click="deskInfo.status++">{{ $t('hc.sit') }}</a-button>
+        <a-button v-if="deskInfo.status === 0" @click="deskInfo.status++">{{ $t('hc.order') }}</a-button>
+        <a-button v-if="deskInfo.status === 1" @click="deskInfo.status++">{{ $t('hc.serve') }}</a-button>
+        <a-button v-if="deskInfo.status === 2" @click="deskInfo.status++">{{ $t('hc.pay') }}</a-button>
+        <a-button v-if="deskInfo.status === 3" @click="deskInfo.status = -1">{{ $t('hc.clean') }}</a-button>
       </template>
-      <a-button class="desk" size="large">19</a-button>
-      <a-button class="chair No1" shape="circle"></a-button>
+      <a-button class="desk" size="large" :class="mapClass(deskInfo.status)">{{ deskInfo.id }}</a-button>
+      <a-button v-for="n in deskInfo.seats" class="chair" :class="`No${n}`" shape="circle"></a-button>
     </a-popover>
   </div>
 </template>
@@ -14,16 +30,34 @@
 <script>
 export default {
   name: 'Table',
-  props: ['deskInfo']
+  props: ['deskInfo'],
+  methods: {
+    mapClass(tableStatus) {
+      switch (tableStatus) {
+        case -1:
+          return ''
+        case 0:
+          return 'error'
+        case 1:
+          return 'warning'
+        case 2:
+          return 'ok'
+        case 3:
+          return 'error'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
 .table-container {
+  margin: 20px;
   position: relative;
   padding: 20px;
   width: 90px;
   height: 90px;
+  display: inline-block;
 
   .desk {
     display: flex;
@@ -46,9 +80,53 @@ export default {
       left: 0;
     }
 
+    &.No2 {
+      top: 45px;
+      left: 0;
+    }
 
+    &.No3 {
+      top: 22.5px;
+      left: 70px;
+    }
+
+    &.No4 {
+      top: 45px;
+      left: 70px;
+    }
+
+    &.No5 {
+      top: 0;
+      left: 22.5px;
+    }
+
+    &.No6 {
+      top: 0;
+      left: 45px;
+    }
+
+    &.No7 {
+      top: 70px;
+      left: 22.5px;
+    }
+
+    &.No8 {
+      top: 70px;
+      left: 45px;
+    }
   }
 }
 
+.warning {
+  color: #d9d903;
+}
+
+.ok {
+  color: #029b02;
+}
+
+.error {
+  color: red;
+}
 
 </style>
--- src/HeyCafe/pages/Overview.vue
+++ src/HeyCafe/pages/Overview.vue
@@ -1,59 +1,146 @@
 <template>
   <div class="cafe-overview">
-    <a-carousel arrows>
-      <template #prevArrow>
-        <div class="custom-slick-arrow" style="left: 10px; zindex: 1">
-          <LeftCircleOutlined />
-        </div>
-      </template>
-      <template #nextArrow>
-        <div class="custom-slick-arrow" style="right: 10px">
-          <right-circle-outlined />
-        </div>
-      </template>
-      <div class="table-status">
-        <Table></Table>
+    <a-carousel :arrows="true" class="table-status">
+      <div
+        slot="prevArrow"
+        slot-scope="props"
+        class="custom-slick-arrow"
+        style="left: -40px;z-index: 1"
+      >
+        <a-icon type="left-circle" />
       </div>
-      <div><h3>2</h3></div>
-      <div><h3>3</h3></div>
-      <div><h3>4</h3></div>
+      <div slot="nextArrow" slot-scope="props" class="custom-slick-arrow" style="right: -40px">
+        <a-icon type="right-circle" />
+      </div>
+      <div class="table-page">
+        <Table v-for="item of desks" v-if="item.page===1" :key="item.id" :desk-info.sync="item"
+               @remove-table="handleRemoveTable($event)"></Table>
+      </div>
+      <div>
+        <Table v-for="item of desks" v-if="item.page===2" :key="item.id" :desk-info.sync="item"
+               @remove-table="handleRemoveTable($event)"></Table>
+      </div>
     </a-carousel>
   </div>
 </template>
 
 <script>
 import Table from '@/HeyCafe/components/Table'
-import { LeftCircleOutlined } from '@ant-design/icons-vue';
 
 export default {
   name: 'Overview',
   data() {
     return {
-      desks: [{
-        orderId: 0,
-        id: 2,
-        seats: 4,
-        page: 1,
-        status: 0,
-        dishes: null,
-        dishMap: null,
-        waiterId: 1,
-        cusId: 0
-      }]
+      desks: [
+        {
+          orderId: 0,
+          id: 1,
+          seats: 4,
+          page: 1,
+          status: 0,
+          waiterId: 1,
+          cusId: 0
+        },
+        {
+          orderId: 0,
+          id: 2,
+          seats: 5,
+          page: 1,
+          status: 1,
+          waiterId: 1,
+          cusId: 0
+        },
+        {
+          orderId: 0,
+          id: 3,
+          seats: 7,
+          page: 1,
+          status: 2,
+          waiterId: 1,
+          cusId: 0
+        },
+        {
+          orderId: 0,
+          id: 4,
+          seats: 8,
+          page: 1,
+          status: 3,
+          waiterId: 1,
+          cusId: 0
+        },
+        {
+          orderId: 0,
+          id: 5,
+          seats: 4,
+          page: 2,
+          status: 0,
+          waiterId: 1,
+          cusId: 0
+        },
+        {
+          orderId: 0,
+          id: 6,
+          seats: 5,
+          page: 2,
+          status: 1,
+          waiterId: 1,
+          cusId: 0
+        },
+        {
+          orderId: 0,
+          id: 7,
+          seats: 7,
+          page: 2,
+          status: 2,
+          waiterId: 1,
+          cusId: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    handleRemoveTable(id) {
+      this.desks = this.desks.filter((item) => {
+        return item.id !== id
+      })
     }
   },
   components: {
-    Table,
-    LeftCircleOutlined
+    Table
   }
 }
 </script>
+
+<style scoped>
+.ant-carousel >>> .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #fff;
+  opacity: 0.5;
+}
+
+.ant-carousel >>> .custom-slick-arrow:before {
+  display: none;
+}
+
+.ant-carousel >>> .custom-slick-arrow:hover {
+  opacity: 0.7;
+}
+
+</style>
 
 <style scoped lang="less">
 .table-status {
   height: 500px;
   width: 100%;
-  background: rgba(255, 255, 255, 1);
-  //box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+  background: rgba(204, 204, 204, .5);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+  padding: 0 60px;
+}
+
+.table-page {
+  height: 500px;
+  width: 100%;
 }
 </style>
